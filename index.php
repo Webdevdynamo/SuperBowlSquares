034231e12bae7441f1d2159ea3691388331896d0
Webdevdynamo <31548004+Webdevdynamo@users.noreply.github.com>
2026-01-29 08:58:52 -0700

    sfgsdgf

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="shortcut icon" type="image/png" href="./images/favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>NFL Squares Live</title>
     <link rel="stylesheet" href="style.css">
